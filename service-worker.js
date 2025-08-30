8f6be7ae7d2da4813d8e4b9ed70fb074c1b3bb59
Lukman <lukmannrhkm80@gmail.com>
2025-08-30 16:17:18 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,22 +3,16 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap',
-  'https://fonts.gstatic.com',
-  'https://cdn.jsdelivr.net/npm/chart.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js',
-  'https://raw.githubusercontent.com/Lukman-nurhakim-LuDes/coupleadventure/main/icons/icon-192.png',
-  'https://raw.githubusercontent.com/Lukman-nurhakim-LuDes/coupleadventure/main/icons/icon-512.png',
-  'https://raw.githubusercontent.com/Lukman-nurhakim-LuDes/coupleadventure/main/icons/background.png'
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/icons/background.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
+        console.log('Cache terbuka');
         return cache.addAll(urlsToCache);
       })
   );
@@ -33,20 +27,5 @@ self.addEventListener('fetch', event => {
         }
         return fetch(event.request);
       })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
